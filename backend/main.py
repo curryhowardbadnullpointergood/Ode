f0ce4f68de3b2ae75997ad535e9370e1e82c5862
novaxxxzzz <163147358+novaxxxzzz@users.noreply.github.com>
2024-11-18 16:11:43 +0000

    Update main.py for login logout

--- backend/main.py
+++ backend/main.py
@@ -5,11 +5,15 @@ from dotenv import load_dotenv
 import firebase_admin
 from service.register import register_user
 from service.edit import edit_user
+from service.login import login_user
+from service.logout import logout_user
+
 
 load_dotenv()
 app = Flask(__name__)
-cred = credentials.Certificate(os.environ.get('GOOGLE_APPLICATION_CREDENTIALS'))
+cred = credentials.Certificate("C:/Users/user/Documents/cs/cs 3/cloud app/group project/Music-Event-App/backend/music-event-442109-firebase-adminsdk-ib5vx-0abef8c709.json")
 firebase_admin.initialize_app(cred)
+print("Firebase initialized successfully")
 database = firestore.client()
 users_container = database.collection('users')
 
@@ -20,8 +24,10 @@ def user_controller(action):
     # Call different functions based on the path
     if action == "register" and method == 'POST':
         return register_user(request, users_container)
-    elif action == "login":
-        return
+    elif action == "login" and method == 'POST':
+        return login_user(request, users_container)
+    elif action == "logout" and method == 'POST':
+        return logout_user()
     elif action == "edit" and method == 'PUT':
         return edit_user(request, users_container)
     else:
