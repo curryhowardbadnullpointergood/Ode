78d10a7ec8955f2cfa8e1a13deaf2fc3e5280fe6
Ash <darkovl10@gmail.com>
2024-12-19 18:22:17 +0000

    added all the basic routes for now, can change in the future

--- music-events/src/App.jsx
+++ music-events/src/App.jsx
@@ -7,6 +7,18 @@ function App() {
     <Router>
       <Routes>
         <Route path="/login" element={<Login />} />
+        <Route path="/profile" element={<Login />} />
+        {/* think og like a instagram like profile page  */}
+        <Route path="/home" element={<Login />} />
+        {/* sort of like a facebook home page, maybe? to be decided tbh  */}
+        <Route path="/register" element={<Login />} />
+        {/* simple modern register page  */}
+        <Route path="/chat" element={<Login />} />
+        {/* this is the chat/group chat, think of discord like, might not get done tbh  */}
+        <Route path="/videofeed" element={<Login />} />  
+        {/* this is the video add sort of thing  */}
+        <Route path="/" element={<div> Hello world</div>} />
+        {/* this is to prank the slackers when they try run npm start and see the front end not exist lol  */}
       </Routes>
     </Router>
   );
