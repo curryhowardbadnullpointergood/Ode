9f556dc634e2700d2d20aff143cad3a2b15151b1
Ash <darkovl10@gmail.com>
2024-12-19 00:39:18 +0000

    basic login page structure has been done

--- music-events/src/pages/login/login.jsx
+++ music-events/src/pages/login/login.jsx
@@ -2,7 +2,29 @@ import "./login.scss"
 
 const Login = () => {
     return(
-        <div className="login"> Login</div>
+        <div className="login"> 
+        <div className="box">
+            
+        <div className="left">
+            <h1> Welcome! </h1>
+            <p> A bit about our app! Something like oh this is a music app about events, needs to be catchy and short, but informative and formal.</p>
+            <span> Click here to register! </span>
+            <button>Register</button>
+
+        </div>
+
+        <div className="right">
+            <h1> Login</h1>
+            <form> 
+                <input type="text" placeholder="Enter Username: "/>
+                <input type="password" placeholder="Enter Password: "/>
+                <button>Login</button>
+                {/* in  the future it goes without saying this wording needs to be better optimised, for the profs*/}
+            </form>
+        </div>
+
+        </div>
+        </div>
     )
 }
 
