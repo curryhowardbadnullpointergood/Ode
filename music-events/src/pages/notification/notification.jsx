cf83d9da9a5db991f4cc3d77277a5d59777b936c
Ash <darkovl10@gmail.com>
2024-12-24 19:26:03 +0000

    added the notifications page, it is now complete, though someone needs to integrate this with the backend

--- music-events/src/pages/notification/notification.jsx
+++ music-events/src/pages/notification/notification.jsx
@@ -1,5 +1,5 @@
 import "./notification.scss"
-
+import Organisation_Image from "../../assets/org_image.jpg"
 
 // id her is the id of the user, and we get the notifications of the user 
 // from the database 
@@ -9,11 +9,30 @@ import "./notification.scss"
 const notification = () => {
 
     // this is temporary, make sure to make this dynamic in the future 
-    
+    const notification = [
+        {
+            id: 12, // this is the id of the user 
+            name: "Concerto",// this is the name of the organisation organising the event 
+            notification: " This should be an ai generated notification about the event in question: blah blah blah blah blah blahblah blah blahblah blah blahblah blah blahblah blah blahblah blah blahblah blah blahblah blah blahblah blah blahblah blah blahblah blah blahblah blah blahblah blah blahblah blah blahblah blah blah ",
+            image: Organisation_Image, // this should be organisation image query with name 
+
+        }
+    ]
+
   return (
     <div className='notification'>
         
-        div.
+        {notification.map(notifi => (
+            <div className="organisation" key={notifi.id}>
+                <div className="info">
+                    <img src={notifi.image} alt="" />
+                    <span> {notifi.name}</span>
+                </div>
+                <div className="text">
+                    <p>{notifi.notification}</p>
+                </div>
+            </div>
+        ))}
         
     </div>
   )
