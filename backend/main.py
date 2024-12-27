c1a72a73ed8c3c4730a926ed85b0109077db7b8c
AnnaMon56 <161605338+AnnaMon56@users.noreply.github.com>
2024-12-27 16:54:48 +0200

    added interests to profiles & view interests

--- backend/main.py
+++ backend/main.py
@@ -10,7 +10,7 @@ from service.edit import edit_user
 from service.login import login_user
 from service.logout import logout_user
 from service.delete import delete_user
-from service.create_profile import create_profile
+from service.create_profile import create_profile, view_interests
 from service.event import report_user, block_user, create_event, get_users_by_event_id, view_event, filter_by_genre
 from service.utils import store_image
 from service.chat import store_message, store_messages
@@ -84,6 +84,8 @@ def user_process(action, container):
         return create_profile(request, container)
     elif action == "image" and method == 'POST':
         return store_image(request, bucket)
+    elif action == "view_interests" and method == 'GET':
+        return view_interests(request, container)
     else:
         return jsonify({"error": f"Unknown action: {action}"}), 404
 
