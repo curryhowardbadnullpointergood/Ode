0899e4c36afb187cd55612e390c21b2cfd1b860f
TLucas <lucastai0601@gmail.com>
2024-12-29 18:17:24 +0000

    adding a friend_list to display the friends in the profile

--- music-events/src/apiFunctions/HandleUserInfo.jsx
+++ music-events/src/apiFunctions/HandleUserInfo.jsx
@@ -2,16 +2,35 @@ import axios from "axios";
 import {useEffect, useState} from "react";
 export default async function HandleUserInfo(id, setUserData) {
     const path = 'http://localhost:8080/user/view_user';
+    const reply = {  
+        username : "",
+        name: "",
+        profile_picture: "",
+        interests: [],
+        events_interested : [],
+        friends : [],
+        bio : "testing"
+      };
     useEffect (() => {
         const fetchData = async() => {
-            try{    
+            try{ 
                 const response = await axios.post(path,{"username" : id} );
                 //console.log("response: ", response);
+                //console.log("response.data.data: ", response.data.data);
+                const dataObject = response.data.data;
                 if (response.data.status === "success"){
-                    setUserData(response.data.data);
+                    for (const key of Object.keys(dataObject)){
+                        if (key in reply){
+                            //console.log("hi");
+                            reply[key] = dataObject[key];
+                        }
+                    }
+                    setUserData(reply);
+                   
                 }
                 else{
                     setUserData(response.data.error);
+                    
                 }
             }
             catch(error){
--- music-events/src/pages/profile/profile.jsx
+++ music-events/src/pages/profile/profile.jsx
@@ -2,6 +2,7 @@ import "./profile.scss";
 import { useLoaderData, useParams, withRouter } from "react-router";
 import {useState} from "react";
 import HandleUserInfo from "../../apiFunctions/HandleUserInfo";
+import Friend_list from "./popup_screen/Friend_list";
 
 
 // this should be linked back to the profile of the person in question 
@@ -17,19 +18,38 @@ import Sophie from "../../assets/anne-sophie-mutter_profile.jpg"
 const Profile = (props) => {
     const params = useParams();
 	const [userData, setUserData] = useState({});
-    HandleUserInfo(params.id,setUserData);
     let exist = true;
-    console.log(userData);
-    if (userData === "User not found"){
+    const response = HandleUserInfo(params.id,setUserData);
+
+    // the following is the variable controlling the showing. 
+    // Can be removed once everything is tested but for simpicity we just connecting the info from api to them first: Lucas
+    let interests = ["test1","test2","test3"];
+    let friends = ["friend1", "friend2", "friend3", "friend4", "friend5" ];
+    let name = "name";
+    let nickname = "nickname";
+    let bio = "Anne-Sophie Mutter (born 29 June 1963) is a German violinist. Born and raised in Rheinfelden, Baden-Württemberg, Mutter started playing the violin at age five and continued studies in Germany and Switzerland. She was supported early in her career by Herbert von Karajan and made her orchestral debut with the Berlin Philharmonic in 1977. Since Mutter gained prominence in the 1970s and 1980s, she has recorded over 50 albums, mostly with the Deutsche Grammophon label, and performed as a soloist with leading orchestras worldwide and as a recitalist. Her primary instrument is the Lord Dunn–Raven Stradivarius violin\.Mutter's repertoire includes traditional classical violin works from the Baroque period to the 20th century, but she also is known for performing, recording, and commissioning new works by present-day composers. As an advocate of contemporary music, she has had several works composed especially for her, by Thomas Adès, Unsuk Chin, Sebastian Currier, Henri Dutilleux, Sofia Gubaidulina, Witold Lutosławski, Norbert Moret, Krzysztof Penderecki, André Previn, Wolfgang Rihm, Jörg Widmann, and John Williams. Mutter has received numerous awards and prizes, including four Grammy Awards (1994, 1999, 2000, and 2005), Echo Klassik awards (2009, 2014), the Grand Decoration of Honour of Austria (2007), the Grand Cross Order of Merit of the Federal Republic of Germany (2009), France\'s Legion of Honour (2009), Spain\'s Gold Medal of Merit in the Fine Arts (2016), Romania\'s Grand Cross National Order of Merit (2017), Poland\'s Gold Medal for Merit to Culture – Gloria Artis (2018), Japan\'s Praemium Imperiale (2019), the Polar Music Prize (2019), and holds honorary memberships at the Royal Academy of Music (1986) and American Academy of Arts and Sciences (2013)\.Mutter founded the Association of Friends of the Anne-Sophie Mutter Foundation e.V. in 1997 and the Anne-Sophie Mutter Foundation in 2008, which support young string musicians. She frequently gives benefits concerts and, since 2021, has been the president of the German Cancer Aid. "
+    
+    
+    // the data retrieved from api will have time delay that making userData empty here. Should have a loading screen before it arrives
+    if (userData === "User not found" || userData.name === undefined){ // user not found or the data hasn't arrived yet
         exist = false;
+        console.log("undefined");
     }
-	console.log(exist);
+    else{ // data arrives
+        console.log(userData);
+        console.log("userData[\"interests\"]: ",userData["interests"]);
+        interests.push(...userData["interests"]);
+        bio = userData["bio"];
+
+    }
+
 
     function User_exist({ name, exist }) {
         if (!exist) {
           return null;
         }
-        return <li className="item">{name}</li>;
+       
+        return <li className="item">{name + "hi"}</li>;
       }
 
     return(
@@ -42,19 +62,20 @@ const Profile = (props) => {
             <img src={Sophie} alt="" className="profile" />
         </div>
         <div className="personalinformation">
-            <span> {userData.username}</span>  
-
+            <h1>{userData.username}</h1> {/*displaying username*/}
+            <span> {name}</span>  {/*displaying name*/}
+            <Friend_list list = {friends} />
             <div className="bio">
                 <p>
-                Anne-Sophie Mutter (born 29 June 1963) is a German violinist. Born and raised in Rheinfelden, Baden-Württemberg, Mutter started playing the violin at age five and continued studies in Germany and Switzerland. She was supported early in her career by Herbert von Karajan and made her orchestral debut with the Berlin Philharmonic in 1977. Since Mutter gained prominence in the 1970s and 1980s, she has recorded over 50 albums, mostly with the Deutsche Grammophon label, and performed as a soloist with leading orchestras worldwide and as a recitalist. Her primary instrument is the Lord Dunn–Raven Stradivarius violin.
-
-Mutter's repertoire includes traditional classical violin works from the Baroque period to the 20th century, but she also is known for performing, recording, and commissioning new works by present-day composers. As an advocate of contemporary music, she has had several works composed especially for her, by Thomas Adès, Unsuk Chin, Sebastian Currier, Henri Dutilleux, Sofia Gubaidulina, Witold Lutosławski, Norbert Moret, Krzysztof Penderecki, André Previn, Wolfgang Rihm, Jörg Widmann, and John Williams.
-
-Mutter has received numerous awards and prizes, including four Grammy Awards (1994, 1999, 2000, and 2005), Echo Klassik awards (2009, 2014), the Grand Decoration of Honour of Austria (2007), the Grand Cross Order of Merit of the Federal Republic of Germany (2009), France's Legion of Honour (2009), Spain's Gold Medal of Merit in the Fine Arts (2016), Romania's Grand Cross National Order of Merit (2017), Poland's Gold Medal for Merit to Culture – Gloria Artis (2018), Japan's Praemium Imperiale (2019), the Polar Music Prize (2019), and holds honorary memberships at the Royal Academy of Music (1986) and American Academy of Arts and Sciences (2013).
-
-Mutter founded the Association of Friends of the Anne-Sophie Mutter Foundation e.V. in 1997 and the Anne-Sophie Mutter Foundation in 2008, which support young string musicians. She frequently gives benefits concerts and, since 2021, has been the president of the German Cancer Aid. 
+                {bio}
                 </p>
             </div>
+            <span>Interests</span> {/*The part showing the interest of this player. Need styling */}
+            <ul>
+                {interests.map((fav) => (
+                    <li key={fav}>{fav}</li>
+                ))}
+            </ul>
         </div>
         </div>
     )
