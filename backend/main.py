dfc2ab36def839e39e2aeaa1be1fbc3ca1425dfa
Yeegeeeeee <106682971+Yeegeeeeee@users.noreply.github.com>
2025-01-02 02:23:21 +0000

    add subscribing event function

--- backend/main.py
+++ backend/main.py
@@ -11,7 +11,7 @@ from service.login import login_user
 from service.logout import logout_user
 from service.delete import delete_user
 from service.create_profile import create_profile, view_interests, view_user
-from service.event import report_user, block_user, create_event, get_users_by_event_id, view_event, filter_by_genre
+from service.event import report_user, block_user, create_event, get_users_by_event_id, view_event, filter_by_genre, subscribing_event
 from service.utils import store_image
 from service.newchat import store_message, store_messages, store_images
 from service.friend_request import send_friend_request, receive_friend_request, add_friend, view_friend_requests
@@ -56,6 +56,8 @@ def event_controller(action):
         return view_event(request, event_container)
     elif action == "filter_genre" and method == 'POST':
         return filter_by_genre(request, event_container)
+    elif action == "follow" and method == 'POST':
+        return subscribing_event(request, event_container)
 
 
 @app.route('/chat/<path:action>', methods=['GET', 'POST', 'PUT', 'DELETE'])
